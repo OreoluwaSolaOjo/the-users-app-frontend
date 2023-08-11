e6077f04bf58919372efb21bf0dc6475c259546a
OreoluwaSolaOjo <oreoluwasolaojo@gmail.com>
2023-08-11 07:53:34 +0100

    new c

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Login from './components/Login';
+import InputForm from './components/InputForm';
+import PercentageCalculator from './components/PercentageCalculator';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { initializeApp } from 'firebase/app';
+import { config } from './firebase';
+import AuthRoute from './components/AuthRoute';
+import AdminPage from './components/Admin';
 
+
+initializeApp(config.firebaseConfig);
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [formData, setFormData] = useState<any>(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //     {!loggedIn && <Login onLogin={() => setLoggedIn(true)} />}
+    //     {loggedIn && !formData && <InputForm onFormSubmit={(data) => setFormData(data)} />}
+    //     {formData && <PercentageCalculator numOfUsers={formData.numOfUsers} numOfProducts={formData.numOfProducts} />}
+    // </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/login"
+          element={
+            <Login onLogin={() => setLoggedIn(true)} />
+          }
+        />
+        <Route path="/" element={<AuthRoute>
+          <InputForm onFormSubmit={(data) => setFormData(data)} />
+        </AuthRoute>} />
+        <Route path="/admin" element={<AuthRoute>
+          <AdminPage  />
+        </AuthRoute>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
